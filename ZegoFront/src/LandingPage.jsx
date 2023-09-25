b08892908ba8519d2e14580686e64cff6486bf9b
Cesars2130 <cesars.213008@gmail.com>
2023-09-25 00:05:27 -0600

    primera prueba

---
+++ ZegoFront/src/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LandigPage() {
+    <>
+    <div>
+        <h1>Hola</h1>
+    </div>
+    </>
+    
+}
---
+++ ZegoFront/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LandigPage from './LandingPage.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LandigPage/>
+  </React.StrictMode>,
+)
